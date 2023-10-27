aaa352ceab4f44ecf0fbc163f7fe3a4ba048a15b
Irina Tsoi <irinatsoi312@gmail.com>
2023-10-28 01:18:01 +0600

    feat: created react/vue project, installed all dependecies and had set configurations

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <h1 id="headingID">Hello World</h1>;
+  }
+}
+
+export default App;
