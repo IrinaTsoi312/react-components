36c8952e6c4d6ca0b532cae8020948d40216f056
Irina Tsoi <irinatsoi312@gmail.com>
2023-10-29 23:44:58 +0600

    fix: fix scripts for eslint and prettier in package.json

---
+++ src/features/card/Card.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prefer-stateless-function */
+import { Component } from "react";
+
+export default class Card extends Component {
+  render() {
+    return <div>Card</div>;
+  }
+}
