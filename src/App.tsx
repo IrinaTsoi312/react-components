856564e9439af1a92a5bafa88f422e374f531ebb
Irina Tsoi <irinatsoi312@gmail.com>
2023-10-30 03:52:08 +0600

    feat: created SearchPanel component and added styles; created header inside main tag in App component with styles and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { Component } from "react";
 import "./App.css";
+import Logo from "./assets/logo.svg";
 import SearchResults from "./features/search-result/SearchResult";
+import SearchPanel from "./features/search-panel/SearchPanel";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <main>
+        <header className="header">
+          <div className="logo-block">
+            <a href="/">
+              <img src={Logo} alt="logo" className="logo" />
+            </a>
+            <p className="logo-name">
+              Star Wars movies <span className="sub-logo">wiki</span>
+            </p>
+          </div>
+        </header>
+        <SearchPanel />
         <SearchResults />
-      </div>
+      </main>
     );
   }
 }
